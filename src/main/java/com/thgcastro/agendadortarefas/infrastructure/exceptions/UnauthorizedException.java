ea1eeb0b1c2be9db827f883d125ee9c47ff2b6bf
Thiago <nascimentocastro989@gmail.com>
2026-03-22 19:16:19 -0300

    exceção global adicionada

---
+++ src/main/java/com/thgcastro/agendadortarefas/infrastructure/exceptions/UnauthorizedException.java
@@ -0,0 +1,13 @@
+package com.thgcastro.agendadortarefas.infrastructure.exceptions;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class UnauthorizedException extends AuthenticationException {
+
+    public UnauthorizedException(String mensagem) {
+        super(mensagem);
+    }
+    public UnauthorizedException(String mensagem, Throwable throwable){
+        super(mensagem, throwable);
+    }
+}
